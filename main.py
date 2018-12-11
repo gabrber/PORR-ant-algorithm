69cc554f695d42569e6f780cefc2d4de43d40903
gabrber <gabrielaberecka@gmail.com>
2018-12-11 08:52:36 +0100

    disconenct some nodes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+from random import randrange
 from aco_algorithm import AntColony
 
 #np.random.seed(1000)
@@ -11,15 +12,20 @@ from aco_algorithm import AntColony
 #                      [ 3, 2, 1, 2, np.inf]])
 
 #genrate matrix
-N = 500
+N = 10
 rand_matrix = np.random.random_integers(1,100,size=(N,N))
 rand_dist = (rand_matrix + rand_matrix.T)/2
 for i in range(N):
     rand_dist[i][i] = np.inf
+for i in range((int) (N/10)):
+    j = randrange(0,N-1)
+    k = randrange(0,N-1)
+    rand_dist[j][k] = np.inf
+    rand_dist[k][j] = np.inf
 
 start_time = time.time()
 
-ant_colony = AntColony(rand_dist, 20, 10, 0.95, alpha=1, beta=1)
+ant_colony = AntColony(rand_dist, 20, 100, 0.95, alpha=1, beta=1)
 #ant_colony = AntColony(distances, 1, 1, 5, 0.95, alpha=1, beta=1)
 shortest_path = ant_colony.run(0, 5)
 
