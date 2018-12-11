b4001c35cdb2621614bfee83febe08f24d471df5
gabrber <gabrielaberecka@gmail.com>
2018-12-11 17:26:22 +0100

    update spark test

--- spark/test.py
+++ spark/test.py
@@ -7,16 +7,24 @@ conf.setMaster('spark://gabriela-TM1703:7077')
 conf.setAppName('spark-basic')
 sc = SparkContext(conf=conf)
 
+# ACO code
 import numpy as np
-
+from random import randrange
 from aco_algorithm import AntColony
 
-distances = np.array([[np.inf, 1, 1.5, 1],
-                      [1, np.inf, 1, 1.5],
-                      [1.5, 1, np.inf, 1],
-                      [1, 1.5, 1, np.inf]])
+N = 10
+rand_matrix = np.random.random_integers(1,100,size=(N,N))
+rand_dist = (rand_matrix + rand_matrix.T)/2
+for i in range(N):
+    rand_dist[i][i] = np.inf
+for i in range((int) (N/10)):
+    j = randrange(0,N-1)
+    k = randrange(0,N-1)
+    rand_dist[j][k] = np.inf
+    rand_dist[k][j] = np.inf
 
+ant_colony = AntColony(rand_dist, 20, 100, 0.95, alpha=1, beta=1)
+shortest_path = ant_colony.run(0, 5)
 
-ant_colony = AntColony(distances, 1, 1, 100, 0.95, alpha=1, beta=1)
-shortest_path = ant_colony.run()
+print("--- Final result is: ---")
 print(shortest_path)
